38e179354a04eb5987a1cad2af175e0bb9f371a4
celeste <152419808+nothingnessrune@users.noreply.github.com>
2026-02-21 05:55:56 +0900

    유니코드 공백으로 인한 path 오류 수정

--- module/file_processor.py
+++ module/file_processor.py
@@ -2,7 +2,6 @@ import re
 
 # 폴더 / 파일 저장시 특수문자 저장용 처리 클래스
 
-
 class FileProcessor:
     def remove_forbidden_str(self, string: str) -> str:
         """
@@ -17,10 +16,25 @@ class FileProcessor:
         # \t 과 \n제거 (\t -> 공백 , \n -> 공백)
         table = str.maketrans("\t\n", "  ")
         processed_string = processed_string.translate(table)
-        return processed_string.strip()
+        bprocessed_string = self.soft_strip_edges(processed_string)
+        return bprocessed_string.strip().rstrip(".")
 
     def remove_tag(self, string: str) -> str:
         # <tag>, &nbfs 등등 제거
         cleaner = re.compile("<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});")
         string = re.sub(cleaner, "", string)
         return string
+
+    def soft_strip_edges(self, s: str) -> str:
+        # 경로명 앞 뒤 유니코드 공백 문자 제거
+        WINDOWS_WEIRD_SPACES = [
+            "\u00A0",  # NO-BREAK SPACE
+            "\u200B",  # ZERO WIDTH SPACE
+            "\u2009",  # THIN SPACE
+            "\u200A",  # HAIR SPACE
+            "\u3000",  # IDEOGRAPHIC SPACE
+            "\uFEFF",  # ZERO WIDTH NO-BREAK SPACE (BOM)
+        ]
+        
+        TRIM_CHARS = " \t\r\n" + "".join(WINDOWS_WEIRD_SPACES)
+        return s.strip(TRIM_CHARS)
